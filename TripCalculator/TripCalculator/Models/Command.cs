c826cca9a17e0a1398848ca3b78145a48c94d155
unknown <June Personal@DESKTOP-4TJIT49.attinc.com>
2017-12-07 16:28:12 -0800

    calc-1 add commands to the viewmodel

---
+++ TripCalculator/TripCalculator/Models/Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TripCalculator.Models
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action<object> _commandFunction;
+        private Func<object, bool> _canExecuteCommandFunction;
+
+        public Command(Action<object> commandFunction, Func<object, bool> canExecuteFunction)
+        {
+            _commandFunction = commandFunction;
+            _canExecuteCommandFunction = canExecuteFunction;
+        }
+
+        /// <summary>
+        /// If no function exists, defaults to false (cannot execute)
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteCommandFunction == null ? false : _canExecuteCommandFunction.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+                _commandFunction?.Invoke(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
+
--- TripCalculator/TripCalculator/ViewModels/MainViewModel.cs
+++ TripCalculator/TripCalculator/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TripCalculator.Models;
 
 namespace TripCalculator.ViewModels
 {
@@ -11,10 +12,51 @@ namespace TripCalculator.ViewModels
     /// </summary>
     public interface IMainViewModel
     {
+        Command StartNewTripCommand { get; }
+        Command LoadExistingTripCommand { get; }
+
     }
 
     class MainViewModel : IMainViewModel
     {
+        private Command _loadExistingTripCommand;
+        public Command LoadExistingTripCommand
+        {
+            get
+            {
+                return _loadExistingTripCommand;
+            }
+        }
+
+        private Command _startNewTripCommand;
+        public Command StartNewTripCommand
+        {
+            get
+            {
+                return _startNewTripCommand;
+            }
+        }
+
+        public MainViewModel()
+        {
+            _loadExistingTripCommand = new Command(DoLoadExistingTripCommand, VerifyParameterIsValidPath);
+            _startNewTripCommand = new Command(DoStartNewTripCommand, new Func<object, bool>((obj) => { return true; }));
+        }
+
+        private void DoStartNewTripCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoLoadExistingTripCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool VerifyParameterIsValidPath(object arg)
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
