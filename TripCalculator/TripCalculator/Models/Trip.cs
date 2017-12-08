6b89bd873515aea8d1a8adf9bb97d27b28a4ef77
unknown <June Personal@DESKTOP-4TJIT49.attinc.com>
2017-12-07 16:04:35 -0800

    calc-1 change to observable and notifiable bc we will use MVVM to bind to the models later on

--- TripCalculator/TripCalculator/Models/Trip.cs
+++ TripCalculator/TripCalculator/Models/Trip.cs
@@ -1,21 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TripCalculator.Models
 {
-    public class Trip
+    public class Trip : INotifyPropertyChanged
     {
         public const string DefaultTripName = "My New Trip";
 
-        public List<Traveler> Travelers { get; private set; }
-        public string Name { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        public ObservableCollection<Traveler> Travelers { get; private set; }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
 
         public Trip()
         {
-            Travelers = new List<Traveler>();
+            Travelers = new ObservableCollection<Traveler>();
             Name = DefaultTripName;
         }
 
@@ -31,7 +52,7 @@ namespace TripCalculator.Models
 
         public bool DeleteTraveler(string name)
         {
-            var match = Travelers.Find(t => t.Name == name);
+            var match = Travelers.ToList().Find(t => t.Name == name);
             if(match != null)
                 Travelers.Remove(match);
 
